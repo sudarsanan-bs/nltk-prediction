2dcd9d4fa55890b920406ad19eb8b6de97c60479
sudarsanan-bs <44740932+sudarsanan-bs@users.noreply.github.com>
2019-01-19 16:15:50 +0530

    Create nlp-prediction.py

---
+++ nlp-prediction.py
@@ -0,0 +1,34 @@
+import os
+import nltk
+from nltk.corpus import names
+
+#Classifier engine to predict if a given name is that of a male or that of a female
+#Feature set used is the last letter of name
+
+#Feature extraction method, returns last letter of name
+def gender_features(word):
+    return {'last_letter': word[-1]}
+
+# Load data and training 
+
+path = os.getcwd()
+path = os.path.join(path,'nltk') 
+male = os.path.join(path ,'male.txt')
+female = os.path.join(path ,'female.txt')
+
+male_lines = [line.rstrip('\n') for line in open(male)]
+female_lines = [line.rstrip('\n') for line in open(female)]
+
+names = ([(name, 'male') for name in male_lines] + 
+ 	 [(name, 'female') for name in female_lines])
+
+# Defining feature set
+featuresets = [(gender_features(n), g) for (n,g) in names]
+train_set = featuresets
+
+#Train using Naive Bayes Classifier
+classifier = nltk.NaiveBayesClassifier.train(train_set)
+
+# Predict
+query_name = input("Name: ")
+print(query_name + ' is a ' + classifier.classify(gender_features(query_name)))
